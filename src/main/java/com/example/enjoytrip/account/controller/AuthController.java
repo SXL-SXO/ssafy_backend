67ea1b378577c88cf834b8362565ce1da9929604
khg <pkgprud0707@pukyong.ac.kr>
2024-05-20 05:44:35 +0900

    2020520 - get, requestBody 안되어서 get, requestParam 변경

--- src/main/java/com/example/enjoytrip/account/controller/AuthController.java
+++ src/main/java/com/example/enjoytrip/account/controller/AuthController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 @RestController
 public class AuthController {
+
     private final AccountService accountService;
 
     @GetMapping("/login")
