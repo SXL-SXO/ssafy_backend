ab2680e3df964f2e691a578296ddd60e3a339054
khg <pkgprud0707@pukyong.ac.kr>
2024-05-22 06:15:34 +0900

    2020522 - 이미지저장

--- src/main/java/com/example/enjoytrip/config/WebConfig.java
+++ src/main/java/com/example/enjoytrip/config/WebConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -18,5 +19,10 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedHeaders("*")
                 .allowCredentials(true)
                 .maxAge(3000);
+    }
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/summernoteImage/**")
+                .addResourceLocations("file:///C:/summernote_image/");
     }
 }
